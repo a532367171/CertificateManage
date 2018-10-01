fbbd744c9b4739950ebb355c7d527da80d1e747d
a53236717171 <532367171@qq.com>
2018-10-01 12:43:05 +0800

    pdf帮助类

--- 证书管理/Form1.cs
+++ 证书管理/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,9 +12,28 @@ namespace 证书管理
 {
     public partial class Form1 : Form
     {
+        #region 定义全局变量，并实例化公共类对象
+        public static string strFullName = "";    //记录选择的文件名及路径
+        public static ArrayList list = new ArrayList();    //记录选择的项
+        #endregion
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dlg.Multiselect = true;
+            dlg.Filter = "PDF文件|*.pdf";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string s in dlg.FileNames)
+                {
+                    list.Add(s);
+                    //MessageBox.Show(s);
+                }
+            }
         }
     }
 }
